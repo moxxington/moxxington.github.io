cea12a861c0c1f4bf12b8a9e0eedfac612379cdf
moxxington <moxxington@outlook.com>
2021-07-04 23:37:06 -0700

    Add reset button functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let sales = document.querySelector('#sales')
+let cosd = document.querySelector('#cosd')
+let days = document.querySelector('#daysOpen')
+let hours = document.querySelector('#hoursOpen')
+let goal = document.querySelector('#goal')
+
 // Select button
 const buttonCalc = document.querySelector('.btn-cta')
 const buttonReset = document.querySelector('.btn-reset')
@@ -9,44 +15,44 @@ buttonReset.addEventListener('click', resetValues)
 // Calculate each input value
 function calculateGoals () {
   // Grab all input values
-  let sales = parseInt(document.querySelector('#sales').value)
-  let cosd = parseFloat(document.querySelector('#cosd').value)
-  let days = parseFloat(document.querySelector('#daysOpen').value)
-  let hours = parseFloat(document.querySelector('#hoursOpen').value)
-  let goal = parseFloat(document.querySelector('#goal').value)
-
+  let salesValue = parseInt(sales.value)
+  let cosdValue = parseFloat(cosd.value)
+  let daysValue = parseFloat(days.value)
+  let hoursValue = parseFloat(hours.value)
+  let goalValue = parseFloat(goal.value)
+  
   // Select all outputs
   let weeklySales = document.querySelector('#weeklySales')
   let dailySales = document.querySelector('#dailySales')
   let hourlySales = document.querySelector('#hourlySales')
   let halfHourSales = document.querySelector('#halfHourSales')
-
+  
   let weeklyCOSD = document.querySelector('#weeklyCOSD')
   let dailyCOSD = document.querySelector('#dailyCOSD')
   let hourlyCOSD = document.querySelector('#hourlyCOSD')
   let halfHourCOSD = document.querySelector('#halfHourCOSD')
-
-  const weeklySalesGoal = sales * (1 + goal)
-  const dailySalesGoal = (weeklySalesGoal - sales) / days
-  const weeklyCOSDGoal = cosd * (1 + goal)
-  const dailyCOSDGoal = weeklyCOSDGoal - cosd
-
+  
+  const weeklySalesGoal = salesValue * (1 + goalValue)
+  const dailySalesGoal = (weeklySalesGoal - salesValue) / daysValue
+  const weeklyCOSDGoal = cosdValue * (1 + goalValue)
+  const dailyCOSDGoal = weeklyCOSDGoal - cosdValue
+  
   // Calculate Sales Goals
-  weeklySales.value = weeklySalesGoal
-  dailySales.value = dailySalesGoal
-  hourlySales.value = (dailySalesGoal / hours).toFixed(2)
-  halfHourSales.value = (dailySalesGoal / hours / 2).toFixed(2)
-
+  weeklySales.value = weeklySalesGoal.toFixed(2)
+  dailySales.value = dailySalesGoal.toFixed(2)
+  hourlySales.value = (dailySalesGoal / hoursValue).toFixed(2)
+  halfHourSales.value = (dailySalesGoal / hoursValue / 2).toFixed(2)
+  
   // Calculate COSD Goals
-  weeklyCOSD.value = weeklyCOSDGoal
-  dailyCOSD.value = dailyCOSDGoal
-  hourlyCOSD.value = (dailyCOSDGoal / hours).toFixed(2)
-  halfHourCOSD.value = (dailyCOSDGoal / hours / 2).toFixed(2)
+  weeklyCOSD.value = weeklyCOSDGoal.toFixed(2)
+  dailyCOSD.value = dailyCOSDGoal.toFixed(2)
+  hourlyCOSD.value = (dailyCOSDGoal / hoursValue).toFixed(2)
+  halfHourCOSD.value = (dailyCOSDGoal / hoursValue / 2).toFixed(2)
 }
 
 // Reset input values
 function resetValues () {
-
+  
   sales.value = ''
   cosd.value = ''
   days.value = ''
