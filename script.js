23198014d0f0ad33a1a635546546d662a59d464f
Chris U <moxxington@outlook.com>
2021-11-08 21:45:29 -0800

    Tidy up unused code

--- script.js
+++ script.js
@@ -69,3 +69,18 @@ function resetValues () {
   hourlyCOSD.value = ''
   halfHourCOSD.value = ''
 }
+
+
+function cracklePop() {
+  for(let i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log('CracklePop')
+    } else if(i % 3 === 0) {
+      console.log('Crackle')
+    } else if (i % 5 === 0) {
+      console.log('Pop')
+    } else {
+      console.log(i)
+    }
+  }
+}
